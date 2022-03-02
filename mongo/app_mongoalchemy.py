5215c7da1bc867d0cd92863dfacf07eb84d45f08
vipulsharma <vipulsharma936@gmail.com>
2022-03-02 09:26:40 +0530

    Added CRUD operations

--- mongo/app_mongoalchemy.py
+++ mongo/app_mongoalchemy.py
@@ -5,3 +5,15 @@ dive = Book(title='Dive into Python', author=mark_pilgrim, year=2004)
 
 mark_pilgrim.save()
 dive.save()
+
+author = Author.query.filter(Author.name == 'Mark Pilgrim').first()
+book = Book.query.filter(Book.year == 2004).first()
+
+print("Author name: ", author.name)
+print("Book title: ", book.title)
+
+#author.name = 'Vipul Sharma'
+#author.save()
+
+remove_author = Author.query.filter(Author.name == 'Vipul Sharma').first()
+remove_author.remove()
